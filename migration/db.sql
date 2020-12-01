d4898f60bd3b027e0cc116df8c4cf62015f85c03
peng.yu <peng.yu@yibuyiyin.com>
2020-12-01 19:01:07 +0800

    update sql.

--- migration/db.sql
+++ migration/db.sql
@@ -9,12 +9,12 @@ create table user (
     password varchar(32) not null comment '密码',
     nickname varchar(16) not null comment '昵称',
     phone varchar(16) not null default '' comment '联系电话',
-    exp int unsigned not null default 0 comment '有效期 0=永久',
-    status tinyint unsigned not null default 1 comment '状态 1=激活；2=禁用',
-    is_del tinyint unsigned not null default 2 comment '删除状态 1=是；2=否',
+    expired int unsigned not null default 0 comment '有效期 0=永久',
+    is_disabled tinyint unsigned not null default 0 comment '是否禁用状态 1=是；0=否',
+    is_deleted tinyint unsigned not null default 0 comment '是否删除状态 1=是；0=否',
     salt varchar(32) not null comment '盐',
-    utime timestamp on update current_timestamp comment '更新时间',
-    ctime timestamp default current_timestamp comment '创建时间',
+    update_time datetime on update current_timestamp comment '更新时间',
+    create_time datetime default current_timestamp comment '创建时间',
     key idx_account(account),
     primary key(id)
 ) engine=innoDB charset=utf8mb4 comment '用户表';
@@ -27,9 +27,9 @@ create table app (
     name varchar(32) not null comment '应用名称',
     info varchar(255) not null comment '应用描述',
     link varchar(255) not null default '' comment '应用链接',
-    is_del tinyint unsigned not null default 2 comment '删除状态 1=是；2=否',
-    utime timestamp on update current_timestamp comment '更新时间',
-    ctime timestamp default current_timestamp comment '创建时间',
+    is_deleted tinyint unsigned not null default 2 comment '删除状态 1=是；0=否',
+    update_time datetime on update current_timestamp comment '更新时间',
+    create_time datetime default current_timestamp comment '创建时间',
     key idx_appid(appid),
     primary key(id)
 ) engine=innoDB charset=utf8mb4 comment '应用表';
@@ -39,8 +39,8 @@ create table role (
     id int unsigned not null auto_increment,
     name varchar(32) not null comment '角色名',
     info varchar(255) not null comment '描述',
-    utime timestamp on update current_timestamp comment '更新时间',
-    ctime timestamp default current_timestamp comment '创建时间',
+    update_time datetime on update current_timestamp comment '更新时间',
+    create_time datetime default current_timestamp comment '创建时间',
     primary key(id)
 ) engine=innoDB charset=utf8mb4 comment '角色表';
 
@@ -53,29 +53,44 @@ create table permission (
     router varchar(255) not null comment '路由',
     method varchar(8) not null comment '方法 PUT/DELETE/POST/GET',
     parent_id int unsigned not null default 0 comment '父ID',
-    utime timestamp on update current_timestamp comment '更新时间',
-    ctime timestamp default current_timestamp comment '创建时间',
+    update_time datetime on update current_timestamp comment '更新时间',
+    create_time datetime default current_timestamp comment '创建时间',
     key idx_appid(app_id),
     primary key(id)
 ) engine=innoDB charset=utf8mb4 comment '权限表';
 
-drop table if exists perm_user;
-create table perm_user (
+drop table if exists permission_user;
+create table permission_user (
+    id int unsigned not null auto_increment,
     user_id int unsigned not null comment '用户表ID',
-    perm_id int unsigned not null comment '权限表ID',
-    unique uniq_userid_permid(user_id, perm_id)
+    permission_id int unsigned not null comment '权限表ID',
+    is_forbidden tinyint unsigned not null default 1 comment '是否禁止访问 0=否；1=是',
+    update_time datetime on update current_timestamp comment '更新时间',
+    create_time datetime default current_timestamp comment '创建时间',
+    unique uk_userid_permid(user_id, permission_id),
+    primary key(id)
 ) engine=innoDB charset=utf8mb4 comment '用户权限';
 
-drop table if exists perm_role;
-create table perm_role (
+drop table if exists permission_role;
+create table permission_role (
+    id int unsigned not null auto_increment,
     role_id int unsigned not null comment '角色表ID',
-    perm_id int unsigned not null comment '权限表ID',
-    unique uniq_roleid_permid(role_id, perm_id)
+    permission_id int unsigned not null comment '权限表ID',
+    is_forbidden tinyint unsigned not null default 1 comment '是否禁止访问 0=否；1=是',
+    update_time datetime on update current_timestamp comment '更新时间',
+    create_time datetime default current_timestamp comment '创建时间',
+    unique uk_roleid_permid(role_id, permission_id),
+    primary key(id)
 ) engine=innoDB charset=utf8mb4 comment '角色权限';
 
 drop table if exists role_user;
 create table role_user (
+    id int unsigned not null auto_increment,
     role_id int unsigned not null comment '角色表ID',
     user_id int unsigned not null comment '用户表ID',
-    unique uniq_roleid_userid(role_id, user_id)
+    is_forbidden tinyint unsigned not null default 1 comment '是否禁止访问 0=否；1=是',
+    update_time datetime on update current_timestamp comment '更新时间',
+    create_time datetime default current_timestamp comment '创建时间',
+    unique uk_roleid_userid(role_id, user_id),
+    primary key(id)
 ) engine=innoDB charset=utf8mb4 comment '角色成员表';
